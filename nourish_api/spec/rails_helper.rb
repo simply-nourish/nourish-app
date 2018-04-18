1fcd62b128a10da571966eaafc756776dba7c0e0
Sarah Maas <sarah.ann.maas@gmail.com>
2018-04-17 23:54:05 -0500

    more test configuration

--- nourish_api/spec/rails_helper.rb
+++ nourish_api/spec/rails_helper.rb
@@ -7,6 +7,9 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 
+# require database cleaner at the top level
+require 'database_cleaner'
+
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
@@ -21,6 +24,14 @@ require 'rspec/rails'
 # require only the support files necessary.
 #
 # Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+
+# configure shoulda matchers to use rspec as the test framework and full matcher libraries for rails
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
+end
 
 # Checks for pending migrations and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove this line.
@@ -54,4 +65,20 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+   # add `FactoryBot` methods
+   config.include FactoryBot::Syntax::Methods
+
+   # start by truncating all the tables but then use the faster transaction strategy the rest of the time.
+   config.before(:suite) do
+     DatabaseCleaner.clean_with(:truncation)
+     DatabaseCleaner.strategy = :transaction
+   end
+ 
+   # start the transaction strategy as examples are run
+   config.around(:each) do |example|
+     DatabaseCleaner.cleaning do
+       example.run
+     end
+   end
 end
