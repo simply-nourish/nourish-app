c6b02c682bc2825856f4a56d98a72a554d928524
Sarah Maas <sarah.ann.maas@gmail.com>
2018-04-18 01:46:38 -0500

    switch to mysql

--- nourish_api/config/routes.rb
+++ nourish_api/config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   constraints subdomain: 'api' do
-
   end
 end
+
--- nourish_api/spec/rails_helper.rb
+++ nourish_api/spec/rails_helper.rb
@@ -23,7 +23,11 @@ require 'database_cleaner'
 # directory. Alternatively, in the individual `*_spec.rb` files, manually
 # require only the support files necessary.
 #
-# Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+
+# Checks for pending migrations and applies them before tests are run.
+# If you are not using ActiveRecord, you can remove this line.
+ActiveRecord::Migration.maintain_test_schema!
 
 # configure shoulda matchers to use rspec as the test framework and full matcher libraries for rails
 Shoulda::Matchers.configure do |config|
@@ -32,10 +36,6 @@ Shoulda::Matchers.configure do |config|
     with.library :rails
   end
 end
-
-# Checks for pending migrations and applies them before tests are run.
-# If you are not using ActiveRecord, you can remove this line.
-ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
@@ -66,19 +66,19 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
 
-   # add `FactoryBot` methods
-   config.include FactoryBot::Syntax::Methods
+  # add `FactoryBot` methods
+  config.include FactoryBot::Syntax::Methods
 
-   # start by truncating all the tables but then use the faster transaction strategy the rest of the time.
-   config.before(:suite) do
-     DatabaseCleaner.clean_with(:truncation)
-     DatabaseCleaner.strategy = :transaction
-   end
- 
-   # start the transaction strategy as examples are run
-   config.around(:each) do |example|
-     DatabaseCleaner.cleaning do
-       example.run
-     end
-   end
+  # start by truncating all the tables but then use the faster transaction strategy the rest of the time.
+  config.before(:suite) do
+    DatabaseCleaner.clean_with(:truncation)
+    DatabaseCleaner.strategy = :transaction
+  end
+
+  # start the transaction strategy as examples are run
+  config.around(:each) do |example|
+    DatabaseCleaner.cleaning do
+      example.run
+    end
+  end
 end
