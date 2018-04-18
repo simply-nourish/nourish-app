acbded4f25dbf2c18444c91957ae9ada0d5ab758
Sarah Maas <sarah.ann.maas@gmail.com>
2018-04-18 02:12:05 -0500

    add user model, spec, and factory

---
+++ nourish_api/spec/factories/users.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+  factory :user do
+    first_name { Faker::Name.first_name }
+    last_name { Faker::Name.last_name }
+    email { Faker:: Internet.email }
+    password { Faker::Internet.password }
+    default_servings { Faker::Number.number(1) }
+  end
+end
