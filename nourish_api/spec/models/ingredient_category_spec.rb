eac299a412a0122e0dfe83e32aa133ca8051b64d
Sarah <sarahmaas@users.noreply.github.com>
2018-04-22 11:42:31 -0500

    Update ingredient_category_spec.rb

--- nourish_api/spec/models/ingredient_category_spec.rb
+++ nourish_api/spec/models/ingredient_category_spec.rb
@@ -9,6 +9,6 @@ RSpec.describe IngredientCategory, type: :model do
   it { is_expected.to have_many(:ingredients).dependent(:nullify) }
 
   # name must exist
-  it { is_expected.to validate_presence_of(:name) }
+  it { is_expected.to validate_presence_of :name }
 
 end
