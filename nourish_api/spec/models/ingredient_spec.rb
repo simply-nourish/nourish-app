061c0c259b9cd5a6de5b11126a1e0b026a7dc1d0
Sarah <sarahmaas@users.noreply.github.com>
2018-04-22 11:42:00 -0500

    Update ingredient_spec.rb

--- nourish_api/spec/models/ingredient_spec.rb
+++ nourish_api/spec/models/ingredient_spec.rb
@@ -6,9 +6,9 @@ require 'rails_helper'
 RSpec.describe Ingredient, type: :model do
 
   # must have a name
-  it { is_expected.to validate_presence_of(:name) }
+  it { is_expected.to validate_presence_of :name }
 
   # must belong to one category
-  it { is_expected.to belong_to(:ingredient_category) }
+  it { is_expected.to belong_to :ingredient_category }
 
 end
