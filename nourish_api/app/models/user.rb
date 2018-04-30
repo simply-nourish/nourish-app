27952ab7050442a0fed7634afaabc6dddaffa6b5
Sarah Maas <sarah.ann.maas@gmail.com>
2018-04-29 22:50:33 -0500

    add devise token auth users

--- nourish_api/app/models/user.rb
+++ nourish_api/app/models/user.rb
@@ -1,13 +1,7 @@
-class User < ApplicationRecord
+class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
-
-  # custom profile additions and validate password existence
-  # associations, if any
-
-  # validates values exist
-  validates_presence_of :first_name, :last_name, :email, 
-  :encrypted_password, :default_servings
+         :recoverable, :rememberable, :trackable, :validatable, :omniauthable
+  include DeviseTokenAuth::Concerns::User
 end
--- nourish_api/db/schema.rb
+++ nourish_api/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180429163109) do
+ActiveRecord::Schema.define(version: 20180430033545) do
 
   create_table "dietary_restrictions", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "name"
@@ -46,17 +46,6 @@ ActiveRecord::Schema.define(version: 20180429163109) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "recipes", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string "title"
-    t.bigint "user_id"
-    t.string "source"
-    t.integer "servings"
-    t.text "description"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_recipes_on_user_id"
-  end
-
   create_table "shopping_lists", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "name"
     t.bigint "user_id"
@@ -66,23 +55,35 @@ ActiveRecord::Schema.define(version: 20180429163109) do
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string "email", default: "", null: false
+    t.string "provider", default: "email", null: false
+    t.string "uid", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
+    t.boolean "allow_password_change", default: false
     t.datetime "remember_created_at"
     t.integer "sign_in_count", default: 0, null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string "current_sign_in_ip"
     t.string "last_sign_in_ip"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
+    t.string "nickname"
     t.string "first_name"
     t.string "last_name"
+    t.string "image"
+    t.string "email"
     t.integer "default_servings"
+    t.text "tokens"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
   end
 
   add_foreign_key "ingredients", "ingredient_categories"
