4e569eead70204f872697eef8f97e92ae9850fc8
Sarah <sarahmaas@users.noreply.github.com>
2018-05-14 21:54:30 -0500

    Update db seed to remove hardcoded IDs (#21)
    * recreate db schema
    
    * remove all references to id in seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,59 +6,59 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create!(email: 'user@example.com', 
+user1 = User.create!(email: 'user@example.com', 
             nickname: 'UOne', 
             first_name: 'User', 
             last_name: 'One', 
             password: "monkey67", 
             default_servings: 1)
-User.create!(email: 'foo@bar.com', 
+user2 = User.create!(email: 'foo@bar.com', 
             nickname: 'UTwo', 
             first_name: 'User', 
             last_name: 'Two', 
             password: "gorilla68", 
             default_servings: 1)
 
-IngredientCategory.create!(name: 'condiments')  
-IngredientCategory.create!(name: 'meat')      
-IngredientCategory.create!(name: 'dairy')   
-IngredientCategory.create!(name: 'vegetables')   
-IngredientCategory.create!(name: 'nuts')   
+condiments = IngredientCategory.create!(name: 'condiments')  
+meat = IngredientCategory.create!(name: 'meat')      
+dairy = IngredientCategory.create!(name: 'dairy')   
+vegetables = IngredientCategory.create!(name: 'vegetables')   
+nuts = IngredientCategory.create!(name: 'nuts')   
 
-Ingredient.create!(name: 'salt', ingredient_category_id: 1)
-Ingredient.create!(name: 'flounder', ingredient_category_id: 2)
-Ingredient.create!(name: 'cheddar cheese', ingredient_category_id: 3)
-Ingredient.create!(name: 'sweet potato', ingredient_category_id: 4)
-Ingredient.create!(name: 'turnip greens', ingredient_category_id: 4)
-Ingredient.create!(name: 'salted peanuts', ingredient_category_id: 5)
+salt = Ingredient.create!(name: 'salt', ingredient_category: condiments)
+flounder = Ingredient.create!(name: 'flounder', ingredient_category: meat)
+cheddar_cheese = Ingredient.create!(name: 'cheddar cheese', ingredient_category: dairy)
+sweet_potato = Ingredient.create!(name: 'sweet potato', ingredient_category: vegetables)
+turnip_greens = Ingredient.create!(name: 'turnip greens', ingredient_category: vegetables)
+salted_peanuts = Ingredient.create!(name: 'salted peanuts', ingredient_category: nuts)
 
-Measure.create!(name: 'cup')
-Measure.create!(name: 'lb')
+cup = Measure.create!(name: 'cup')
+lb = Measure.create!(name: 'lb')
 
-Recipe.create!(title: 'fishsticks', summary: 'some good fishsticks', instructions: 'put them in the oven', user_id: 1)
-Recipe.create!(title: 'croque monsieur', summary: 'a cheesy sandwich the whole family can enjoy', instructions: 'order it at a restaurant', user_id: 2)
-Recipe.create!(title: 'candied yams', summary: 'a family treat, great with fishsticks', instructions: 'buy a yam, then candy it', user_id: 2)
-Recipe.create!(title: 'pizza', summary: 'my fave-rave food', instructions: 'ditch the fishsticks, then call 1-800-NO-FSHTK', user_id: 2)
+fishsticks = Recipe.create!(title: 'fishsticks', summary: 'some good fishsticks', instructions: 'put them in the oven', user: user1)
+croque_monsieur = Recipe.create!(title: 'croque monsieur', summary: 'a cheesy sandwich the whole family can enjoy', instructions: 'order it at a restaurant', user: user2)
+candied_yams = Recipe.create!(title: 'candied yams', summary: 'a family treat, great with fishsticks', instructions: 'buy a yam, then candy it', user: user2)
+pizza = Recipe.create!(title: 'pizza', summary: 'my fave-rave food', instructions: 'ditch the fishsticks, then call 1-800-NO-FSHTK', user: user2)
 
 # fishsticks
-IngredientRecipe.create!(ingredient_id: 1, measure_id: 1, recipe_id: 1, amount: 3.5)
-IngredientRecipe.create!(ingredient_id: 2, measure_id: 2, recipe_id: 1, amount: 2)
+IngredientRecipe.create!(ingredient: salt, measure: cup, recipe: fishsticks, amount: 3.5)
+IngredientRecipe.create!(ingredient: flounder, measure: lb, recipe: fishsticks, amount: 2)
 
 # croque monsieur
-IngredientRecipe.create!(ingredient_id: 3, measure_id: 2, recipe_id: 2, amount: 2.5)
-IngredientRecipe.create!(ingredient_id: 4, measure_id: 1, recipe_id: 2, amount: 5)
+IngredientRecipe.create!(ingredient: cheddar_cheese, measure: lb, recipe: croque_monsieur, amount: 2.5)
+IngredientRecipe.create!(ingredient: sweet_potato, measure: cup, recipe: croque_monsieur, amount: 5)
 
 # candied yams
-IngredientRecipe.create!(ingredient_id: 4, measure_id: 1, recipe_id: 3, amount: 100)
+IngredientRecipe.create!(ingredient: sweet_potato, measure: cup, recipe: candied_yams, amount: 100)
 
 # pizza
-IngredientRecipe.create!(ingredient_id: 3, measure_id: 2, recipe_id: 4, amount: 10.25)
-IngredientRecipe.create!(ingredient_id: 6, measure_id: 1, recipe_id: 2, amount: 5)
+IngredientRecipe.create!(ingredient: cheddar_cheese, measure: lb, recipe: pizza, amount: 10.25)
+IngredientRecipe.create!(ingredient: salted_peanuts, measure: cup, recipe: pizza, amount: 5)
 
 # add dietary restrictions
-DietaryRestriction.create!(name: 'vegan')
-DietaryRestriction.create!(name: 'vegetarian')
+vegan = DietaryRestriction.create!(name: 'vegan')
+vegetarian = DietaryRestriction.create!(name: 'vegetarian')
 
 # create a recipe with a dietary restriction
-DietaryRestrictionRecipe.create(recipe_id: 1, dietary_restriction_id: 1)
+DietaryRestrictionRecipe.create(recipe: fishsticks, dietary_restriction: vegan)
 
