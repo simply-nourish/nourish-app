c0c7d6af0103767ef5a8b0a297327f29a7b97326
Colin Powell <colingrahampowell@users.noreply.github.com>
2018-06-01 17:32:57 -0400

    expanding recipe information returned from /meal_plans endpoints (#33)

--- app/serializers/abbrev_recipe_serializer.rb
+++
@@ -1,7 +0,0 @@
-# app/models/serializers/abbrev_recipe_serializer.rb
-# Abbreviated Recipe serializer
-
-class AbbrevRecipeSerializer < ActiveModel::Serializer
-  attributes :id, :title
-end
-  
--- app/serializers/meal_plan_recipe_serializer.rb
+++ app/serializers/meal_plan_recipe_serializer.rb
@@ -3,5 +3,5 @@
 
 class MealPlanRecipeSerializer < ActiveModel::Serializer
   attributes :id, :day, :meal
-  has_one :recipe, :serializer => AbbrevRecipeSerializer
+  has_one :recipe, :serializer => RecipeSerializer
 end
